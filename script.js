6c2a6f3987fc03bfa4db79fb7a878114e8f5cc23
KushdeepSingh <skushdeep5@gmail.com>
2020-11-30 03:35:06 +0530

    bugs fixed

--- script.js
+++ script.js
@@ -6,9 +6,8 @@ let hr = d.getHours();
 let min = d.getMinutes();
 let sec = d.getSeconds();
 hrs = hr * 30 + min * 0.5;
-mins = min * 6 + sec * 0.5;
 small.style.transform = "rotateZ(" + hrs + "deg)";
-long.style.transform = "rotateZ(" + mins + "deg)";
+long.style.transform = "rotateZ(" + min * 6 + "deg)";
 second.style.transform = "rotateZ(" + sec * 6 + "deg)";
 setInterval(() => {
     d = new Date();
@@ -18,9 +17,8 @@ setInterval(() => {
         d = new Date();
         let min = d.getMinutes();
         let hr = d.getHours();
-        mins = min * 6 + sec * 0.5;
         hrs = hr * 30 + min * 0.5;
-        long.style.transform = "rotateZ(" + mins + "deg)";
+        long.style.transform = "rotateZ(" + min * 6 + "deg)";
         small.style.transform = "rotateZ(" + hrs + "deg)";
     },0)
 },1000)
